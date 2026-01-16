9381a95f570ca1c08461f65f2575c00add827373
leila <laila.ahmed.2645@gmail.com>
2026-01-16 15:21:01 +0300

    complete array lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+// index.js
+
+// 1. Initial cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// 2. Destructive functions (modify the original array)
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// 3. Non-destructive functions (return a new array)
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
